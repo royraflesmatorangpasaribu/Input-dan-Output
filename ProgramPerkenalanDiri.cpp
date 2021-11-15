e26ab6a2d54a062de98dfb717512a511f350e33e
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-11-15 17:34:04 +0700

    Program C++

---
+++ ProgramPerkenalanDiri.cpp
@@ -0,0 +1,19 @@
+//Program Perkenalan Diri
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout<<"Hallo Semua, Terima Kasih Sudah Mengunjungi Akun Github saya"<<endl;
+	cout<<"Sebelumnya saya mau memperkenalkan diri saya hehehe"<<endl;
+	cout<<"Nama			: Roy Rafles Matorang Pasaribu"<<endl;
+	cout<<"TTL			: Mulyakencana, 05 Mei 2003"<<endl;
+	cout<<"Cita-cita\t\t: Menjadi Programer yang banyak uang"<<endl;
+	cout<<"Hobi			: Bermain Game Online Mobile "<<endl;
+	cout<<"Motivasi		: Semua akan selesai sebelum deadlinenya jadi jalani aja dulu"<<endl;
+	cout<<"Sekian perkenalan dari saya, terima kasih sudah melihat dan mengunjungi akun github Roy Rafles Matorang Pasaribu"<<endl;
+	cout<<"baybaybay.......salam hangat yaa"<<endl<<endl;
+	cout<<"kunjungi akun media sosial ku ya"<<endl;
+	cout<<"Instagram : royrafmp"<<endl;
+	
+	return 0;
+}
