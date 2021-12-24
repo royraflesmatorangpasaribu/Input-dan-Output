cc9ee0a1b653ab21ffe96f4512d051a8943ffc6d
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-12-24 15:47:49 +0700

    Add files via upload

---
+++ jam.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <ctime>  
+using namespace std;
+int main() {
+   time_t now = time(0);
+   char* dt = ctime(&now);
+   
+   
+   cout<<"================================================="<<endl;
+   cout<<"|        Menampilkan Tanggal dan Waktu          |"<<endl;
+   cout<<"|                 by royrafmp                   |"<<endl;		
+   cout<<"=================================================";
+   cout<<"\nWaktu Sekarang : " << dt;
+
+}
