a0fe9c7785309fcf0d8d135407baee784c64c0c8
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-12-25 22:16:48 +0700

    Update and rename jamdigita;.cpp to jamdigital.cpp

--- jamdigital.cpp
+++ jamdigital.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
-#include <ctime>  
+//library untuk menggunakan ctime
+#include <ctime> 
 using namespace std;
 int main() {
+   //rumus waktu
    time_t now = time(0);
    char* dt = ctime(&now);
 	
