b7b573796644ca620af5500e420cf40cea67136e
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2022-01-21 22:16:36 +0700

    Add files via upload

---
+++ bilanganAcak.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+int main(){
+    int acak1, acak2, acak3, acak4, acak5;
+    acak1 = rand();
+    acak2 = rand();
+    acak3 = rand();
+    acak4 = rand();
+    acak5 = rand();
+    cout << "Bilangan Acak 1 = " << acak1 << endl;
+    cout << "Bilangan Acak 2 = " << acak2 << endl;
+    cout << "Bilangan Acak 3 = " << acak3 << endl;
+    cout << "Bilangan Acak 4 = " << acak4 << endl;
+    cout << "Bilangan Acak 5 = " << acak5 << endl;
+}
